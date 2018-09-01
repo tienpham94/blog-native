7d2ce06ae67ec184b19e1b422576c864b1f213d1
Tien Pham <r3dsky02@yahoo.com.vn>
2018-09-01 22:09:47 +0300

    Add styled-components

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+import { Container } from './styles/grid'
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+      </View>
+    );
+  }
+}
