616cf93f480559135ca38ed6caffc91c6c81fc8b
Tien Pham <r3dsky02@yahoo.com.vn>
2018-09-02 10:48:27 +0300

    Finish login screen

--- screens/Login/Login.js
+++ screens/Login/Login.js
@@ -1,11 +1,33 @@
-import React from 'react'
-import { Text } from 'react-native'
-import { Container } from '../../styles/grid';
+import React from "react";
+import { Text, TextInput, FormControl, Button } from "../../styles";
+import { Title, LoginWrapper, FormContainer } from "./Login.style";
+/**
+ * Remember what I wrote about every component has its own styling. Oh yeah.
+ */
 
-export default function Login(props) {
+export default function Login({ navigation }) {
   return (
-    <Container>
-      <Text>Login Screen</Text>
-    </Container>
-  )
+    <LoginWrapper>
+      <Title h4>Sign in to start blogging</Title>
+      <FormContainer>
+        <FormControl>
+          <TextInput
+            placeholder={"Email address"}
+            keyboardType={"email-address"}
+          />
+        </FormControl>
+
+        <FormControl>
+          <TextInput placeholder={"Password"} secureTextEntry />
+        </FormControl>
+      </FormContainer>
+
+      <Button
+        info
+        title={"Login"}
+        onPress={() => navigation.navigate("Blogs")}
+      />
+      {/* Please remember that Button from react-native is a NATIVE button. It will look different on iOS and Android. And, you can't style it. To have custom style for buttons, we will have to create them ourselves.*/}
+    </LoginWrapper>
+  );
 }
